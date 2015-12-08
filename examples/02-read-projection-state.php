729f1bc1b8feb1089ab1330cae30835c5e9e0b3e
Mike van Riel <mike@ingewikkeld.net>
2015-12-08 07:29:50 +0100

    Add support for travis

---
+++ examples/02-read-projection-state.php
@@ -0,0 +1,31 @@
+<?php
+use PhpInPractice\Matters\Projection\Definition;
+use PhpInPractice\Matters\Projection\ProjectionDeletion;
+use PhpInPractice\Matters\Projections;
+
+include __DIR__ . '/../vendor/autoload.php';
+
+$query = <<<JS
+fromAll().when({
+    'ProjectCreated': function (s,e) {
+        s[e.data.id] = {
+            'id': e.data.id,
+            'name': e.data.name
+        };
+    }
+});
+JS;
+
+$credentials = \PhpInPractice\Matters\Credentials::fromUsernameAndPassword('admin', 'changeit');
+$projectionDefinition = Definition::createNew('example_projects', $query);
+
+$projections = Projections::forUrl('192.168.99.100:2113');
+$projections->create($credentials, $projectionDefinition);
+
+$definition = $projections->get('example_projects');
+var_dump($definition);
+
+var_dump($projections->result($definition));
+
+$projections->delete($credentials, $definition, ProjectionDeletion::SOFT());
+
